f49558ac82601091f63b6f3b019062f91c612e18
Robert <robroyal@gmx.net>
2010-04-19 02:08:13 +0200

    Dungeon Difficulty Bugfix & neues Zeug * Frost Bomb Announcements * Unchained Magic Icons setzen

--- core.lua
+++ core.lua
@@ -5,6 +5,12 @@ local Broadcast = "RAID_WARNING" -- Channel in den gespamt wird
 -- Spell ID des Frost Beacons
 local FrostBeaconID = 70126
 
+-- Spell ID von Unchained Magic
+local UnchainedMagicID = "Unchained Magic"
+
+-- Spellid der Frostbomb im 25er Heroic
+local FrostBombID = 71055
+
 -- Spell IDs des Mystic Buffet
 local MysticBuffetID = {
 	[72528] = true,
@@ -35,17 +41,27 @@ SindraGOZER:SetScript("OnEvent",
 				-- Mystic Buffet
 				if MysticBuffetID[spellID] and amount == 6 then
 					self:MysicBuffetAnnounce()
-				end				
+				-- Unchained Magic
+				elseif spellID == UnchainedMagicID and difficulty == 4 then				
+					self:UnchainedMagic()				
 				-- Frost Beacon
-				if spellID == FrostBeaconID then
+				elseif spellID == FrostBeaconID then
 					self:Beacons()
 				end
 			elseif type:sub(1,18) == "SPELL_AURA_REMOVED" then
 				spellID, spellName = select(9, ...)
-				if spellID == FrostBeaconID then
+				-- Reset der Icons (Frost Beacon & Unchained Magic)
+				if spellID == FrostBeaconID or spellID == UnchainedMagicID then
 					self:RemoveRaidIcon()
 				end
+			elseif srcName == "Frost Bomb" then
+				spellID, spellName, spellSchool, auraType, amount = select(9, ...)
+				-- Frost Bomb
+				if spellID == FrostBombID then
+					self:FrostBombAnnounce()
+				end
 			end
+			
 		elseif event:sub(1,12) == "ZONE_CHANGED" then			
 			self:CheckZone()
 		elseif event == "ADDON_LOADED" then
@@ -71,10 +87,14 @@ function SindraGOZER:CheckZone()
 	-- http://www.wowwiki.com/API_GetDungeonDifficulty
 	-- http://www.wowwiki.com/API_GetSubZoneText
 	subzone    = GetSubZoneText()
-	difficulty = GetInstanceDifficulty()
+	-- Workaround für die ICC Heroic (http://forums.wowace.com/showthread.php?t=17936)
+	local _, _, diff, _, _, hero, flag = GetInstanceInfo(); 
+	difficulty = (flag and (2-(diff%2)+2*hero)) or diff;
+	-- Testoutput
+	self:message("Difficulty: "..difficulty)
 	-- hässlich, sollte mir mal was anderes einfallen lassen
 	if subzone == "The Frost Queen's Lair" or subzone == "Der Hort der Frostkönigin"  then
-		self:message("Looking for |TInterface\\Icons\\ability_hunter_markedfordeath:16|tFrost Beacons and |TInterface\\Icons\\spell_arcane_arcane03:16|tMysic Buffets!")
+		self:message("Looking for |TInterface\\Icons\\ability_hunter_markedfordeath:16|tFrost Beacons and |TInterface\\Icons\\spell_arcane_arcane03:16|tMystic Buffets!")
 		self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 	else
 		self:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
@@ -123,12 +143,6 @@ function SindraGOZER:Beacons()
 	if #beaconTargets == numBeacons[difficulty] then
 		-- evtl. sollte sowas vorher abgeprüft werden
 		if IsRaidOfficer() or IsRaidLeader() then
-			for n = 1, numBeacons[difficulty], 1 do
-				-- Raidtarget Icons setzen
-				SetRaidTarget(beaconTargets[n],Raidicons[n])
-				-- Whispers
-				self:whisper(Whispers[difficulty][n], beaconTargets[n])
-			end
 			-- HAESSLICH ... muss schöner gehen!
 			if difficulty == 1 then
 				self:channel(format("    ({rt%u}%s)   ({rt%u}%s)",Raidicons[2],beaconTargets[2],Raidicons[1],beaconTargets[1]))
@@ -139,6 +153,39 @@ function SindraGOZER:Beacons()
 				self:channel(format("    ({rt%u}%s)   ({rt%u}%s)",Raidicons[2],beaconTargets[2],Raidicons[1],beaconTargets[1]))
 			elseif difficulty == 4 then
 				self:channel(format("   ({rt%u}%s {rt%u}%s)   ({rt%u}%s {rt%u}%s)   ({rt%u}%s {rt%u}%s)",Raidicons[6],beaconTargets[6],Raidicons[5],beaconTargets[5],Raidicons[4],beaconTargets[4],Raidicons[3],beaconTargets[3],Raidicons[2],beaconTargets[2],Raidicons[1],beaconTargets[1]))
+			end
+			for n = 1, numBeacons[difficulty], 1 do
+				-- Raidtarget Icons setzen
+				SetRaidTarget(beaconTargets[n],Raidicons[n])
+				-- Whispers
+				self:whisper(Whispers[difficulty][n], beaconTargets[n])
+			end
+		else
+			self:message("You need to be leader or assistant to do this.")
+		end
+	end
+end
+
+local unchainedTargets = {} -- Tabelle in der die Spieler reingeschrieben werden
+local lastUnchainedTimestamp = time()
+local unchained_delta = 5
+local numUnchained = {2,6,2,6} --  Index 1-4, entspricht der DungeoDifficulty
+function SindraGOZER:UnchainedMagic()
+	-- Reset der Beacons
+	if #unchainedTargets > numUnchained[difficulty] or difftime(time(),lastUnchainedTimestamp) > unchained_delta then
+		unchainedTargets = wipe(unchainedTargets)
+	end	
+	-- Zeit setzen
+	lastUnchainedTimestamp = time()
+	-- Die Tabelle der Reihenfolge mit Namen der Spieler befüllen
+	unchainedTargets[#unchainedTargets + 1] = dstName
+	-- sobald wir alle Beacons gesammelt haben
+	if #unchainedTargets == numUnchained[difficulty] then
+		-- evtl. sollte sowas vorher abgeprüft werden
+		if IsRaidOfficer() or IsRaidLeader() then
+			for n = 1, numUnchained[difficulty], 1 do
+				-- Raidtarget Icons setzen
+				SetRaidTarget(unchainedTargets[n],Raidicons[n])
 			end
 		else
 			self:message("You need to be leader or assistant to do this.")
@@ -159,6 +206,14 @@ function SindraGOZER:MysicBuffetAnnounce()
 	end	
 end
 
+function SindraGOZER:FrostBombAnnounce()
+	local msg = format("%s died in a Frost Bomb!", dstName)
+	if IsRaidOfficer() or IsRaidLeader() then
+		self:channel(Prefix..msg, "RAID")
+	else
+		self:message(msg)
+	end	
+end
 --  HELPER  ZEUGS
 function SindraGOZER:whisper(msg, name)
    if not (msg and name) then return end   
